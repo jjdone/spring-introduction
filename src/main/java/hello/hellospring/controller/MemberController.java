e846ffd7d28d9b645f5f660f3612b00d41f81799
jjdone <ehdlfaos@gmail.com>
2022-12-29 17:40:59 +0900

    feat: implement spring Bean

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,21 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    /**
+     * 생성자 주입이라고 한다.
+     */
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+
+}
