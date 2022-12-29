e00911e4944cbc93a05a5cdfcaa9ae6b980ea854
jjdone <ehdlfaos@gmail.com>
2022-12-29 18:07:01 +0900

    feat: 회원 가입 화면 구현

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -17,5 +20,18 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new") // GetMapping은 주로 조회할 때 사용, 보통 url를 사용할 때
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 
+    @PostMapping("/members/new") // PostMapping은 주로 데이터를 넘겨줄 때 사용
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
